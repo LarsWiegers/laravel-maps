762375ac8471b1ae981e4f28e2f93152d896c57b
Lars Wiegers <larswiegers@live.nl>
2021-04-22 15:25:36 +0200

    initial commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
