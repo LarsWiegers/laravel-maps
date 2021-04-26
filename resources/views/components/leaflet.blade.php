2a3c5fd5a604c9fbd2dd769861b1fdfa66ccafb3
Lars Wiegers <larswiegers@live.nl>
2021-04-26 16:52:40 +0200

    leaflet icons

--- resources/views/components/leaflet.blade.php
+++ resources/views/components/leaflet.blade.php
@@ -18,7 +18,25 @@
 
     var mymap = L.map('mapid').setView([{{implode(",", $centerPoint)}}], {{$zoomLevel}});
     @foreach($markers as $marker)
-        var marker = L.marker([{{implode(",", $marker)}}]).addTo(mymap);
+
+        @if(isset($marker['icon']))
+
+            let icon = L.icon({
+                iconUrl: '{{$marker['icon']}}',
+                iconSize:     [38, 95], // size of the icon
+                shadowSize:   [50, 64], // size of the shadow
+                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                shadowAnchor: [4, 62],  // the same for the shadow
+                popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            });
+            L.marker([{{$marker['lat'] ?? $marker[0]}}, {{$marker['long'] ?? $marker[1]}}], {icon: icon}).addTo(mymap);
+
+        @else
+
+            L.marker([{{implode(",", $marker)}}]).addTo(mymap);
+
+    @endif
+
     @endforeach
 
     @if($tileHost === 'mapbox')
