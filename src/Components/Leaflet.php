d6cb3db5696f05c9e213ad707bfc5c440e18fdd1
Lars Wiegers <larswiegers@live.nl>
2021-04-26 08:42:33 +0200

    wip

--- src/Components/Leaflet.php
+++ src/Components/Leaflet.php
@@ -8,12 +8,36 @@ use Illuminate\View\View;
 class Leaflet extends Component
 {
 
-    public function __construct()
+    public int $zoomLevel;
+
+    public int $maxZoomLevel;
+
+    public array $centerPoint;
+
+    public array $markers;
+
+    public function __construct($centerPoint = [0,0], $markers = [], $zoomLevel = 13, $maxZoomLevel = 18)
     {
+        $this->centerPoint = $centerPoint;
+        $this->zoomLevel = $zoomLevel;
+        $this->maxZoomLevel = $maxZoomLevel;
+        $this->markers = $markers;
     }
 
     public function render() : View
     {
-        return view('maps::components.leaflet');
+        $markerArray = [];
+
+        foreach($this->markers as $marker) {
+            $markerArray[] = [implode(",", $marker)];
+        }
+
+        return view('maps::components.leaflet', [
+            'centerPoint' => $this->centerPoint,
+            'zoomLevel' => $this->zoomLevel,
+            'maxZoomLevel' => $this->maxZoomLevel,
+            'markers' => $this->markers,
+            'markerArray' => $markerArray
+        ]);
     }
 }
