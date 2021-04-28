6e53240c34ab9032a0cb163483eb9259c9761530
Lars Wiegers <larswiegers@live.nl>
2021-04-28 13:51:24 +0200

    tests are now using the new way

--- tests/Unit/GoogleMapsTest.php
+++ tests/Unit/GoogleMapsTest.php
@@ -4,31 +4,29 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Tests\InteractsWithViews;
 use Tests\TestCase;
 
 final class GoogleMapsTest extends TestCase
 {
+    use InteractsWithViews;
+
     public function test_it_can_render_the_basic_maps_component()
     {
-        $view = view('maps::tests.views.google.basic');
-        $rendered =$view->render();
-
-        $this->assertStringContainsString('<div id="map"></div>', $rendered);
+        $content = $this->getComponentRenderedContent('<x-maps-google />');
+        $this->assertStringContainsString('<div id="map"></div>', $content);
     }
 
     public function test_it_can_render_with_a_centre_point()
     {
-        $view = view('maps::tests.views.google.with-a-centerpoint-set');
-        $rendered =$view->render();
-
-        $this->assertStringContainsString('center: { lat: 52, lng: 5 },', $rendered);
+        $content = $this->getComponentRenderedContent("<x-maps-google :centerPoint=\"['lat' => 52, 'long' => 5]\"></x-maps-google>");
+        $this->assertStringContainsString('center: { lat: 52, lng: 5 },', $content);
     }
 
     public function test_we_can_set_the_zoom_level()
     {
-        $view = view('maps::tests.views.google.with-zoom-set');
-        $rendered =$view->render();
+        $content = $this->getComponentRenderedContent("<x-maps-google :zoomLevel=\"6\"></x-maps-google>");
 
-        $this->assertStringContainsString('zoom: 6', $rendered);
+        $this->assertStringContainsString('zoom: 6', $content);
     }
 }
--- tests/Unit/LeafletTest.php
+++ tests/Unit/LeafletTest.php
@@ -4,31 +4,28 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Tests\InteractsWithViews;
 use Tests\TestCase;
 
 final class LeafletTest extends TestCase
 {
+    use InteractsWithViews;
+
     public function test_it_can_render_the_basic_leaflet_component()
     {
-        $view = view('maps::tests.views.leaflet.basic');
-        $rendered =$view->render();
-
-        $this->assertStringContainsString('<div id="mapid"></div>', $rendered);
+        $content = $this->getComponentRenderedContent('<x-maps-leaflet></x-maps-leaflet>');
+        $this->assertStringContainsString('<div id="mapid"></div>', $content);
     }
 
     public function test_it_can_render_with_a_centre_point()
     {
-        $view = view('maps::tests.views.leaflet.with-a-centerpoint-set');
-        $rendered =$view->render();
-
-        $this->assertStringContainsString('setView([52, 5]', $rendered);
+        $content = $this->getComponentRenderedContent("<x-maps-leaflet :centerPoint=\"['lat' => 52, 'long' => 5]\"></x-maps-leaflet>");
+        $this->assertStringContainsString('setView([52, 5]', $content);
     }
 
     public function test_we_can_set_the_zoom_level()
     {
-        $view = view('maps::tests.views.leaflet.with-zoom-set');
-        $rendered =$view->render();
-
-        $this->assertStringContainsString('setView([0, 0], 6);', $rendered);
+        $content = $this->getComponentRenderedContent("<x-maps-leaflet :zoomLevel=\"6\"></x-maps-leaflet>");
+        $this->assertStringContainsString('setView([0, 0], 6);', $content);
     }
 }
