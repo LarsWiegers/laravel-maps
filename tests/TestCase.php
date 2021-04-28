e59aacc58340834a3d23f9b4d088a966c0f89e27
Lars Wiegers <larswiegers@live.nl>
2021-04-28 13:51:01 +0200

    add in a way to run blade strings and test their output

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Gajus\Dindent\Indenter;
 use Larswiegers\LaravelMaps\LaravelMapsServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
@@ -24,5 +25,39 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getEnvironmentSetUp($app)
     {
         // perform environment setup
+    }
+
+    public function assertComponentRenders(string $expected, string $template, array $data = []): void
+    {
+        $indenter = new Indenter();
+        $indenter->setElementType('h1', Indenter::ELEMENT_TYPE_INLINE);
+        $indenter->setElementType('del', Indenter::ELEMENT_TYPE_INLINE);
+
+        $blade = (string) $this->blade($template, $data);
+        $indented = $indenter->indent($blade);
+        $cleaned = str_replace(
+            [' >', "\n/>", ' </div>', '> ', "\n>"],
+            ['>', ' />', "\n</div>", ">\n    ", '>'],
+            $indented,
+        );
+
+        $this->assertSame($expected, $cleaned);
+    }
+
+    public function getComponentRenderedContent(string $template, array $data = []): string
+    {
+        $indenter = new Indenter();
+        $indenter->setElementType('h1', Indenter::ELEMENT_TYPE_INLINE);
+        $indenter->setElementType('del', Indenter::ELEMENT_TYPE_INLINE);
+
+        $blade = (string) $this->blade($template, $data);
+        $indented = $indenter->indent($blade);
+        $cleaned = str_replace(
+            [' >', "\n/>", ' </div>', '> ', "\n>"],
+            ['>', ' />', "\n</div>", ">\n    ", '>'],
+            $indented,
+        );
+
+        return $cleaned;
     }
 }
